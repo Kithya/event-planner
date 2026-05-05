6af3561d966ccb9f26dd10c35c2945645378a064
Nara Ratanakkithya <narakithya@gmail.com>
2026-05-05 13:21:39 +0700

    fix dynamic rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import DashboardContent from "@/components/DashboardContent";
 import { getSession } from "@/lib/auth/server";
 import { redirect } from "next/navigation";
-import React from "react";
+import { connection } from "next/server";
 
 const DashboardPage = async () => {
+  await connection();
+
   const session = await getSession();
   const userId = session.data?.user.id;
 
--- app/events/[eventId]/page.tsx
+++ app/events/[eventId]/page.tsx
@@ -1,12 +1,15 @@
 import EventDetailContent from "@/components/EventDetailContent";
 import { getSession } from "@/lib/auth/server";
 import { redirect } from "next/navigation";
+import { connection } from "next/server";
 
 export default async function EventDetailPage({
   params,
 }: {
   params: Promise<{ eventId: string }>;
 }) {
+  await connection();
+
   const { eventId } = await params;
   const session = await getSession();
   const userId = session.data?.user.id;
